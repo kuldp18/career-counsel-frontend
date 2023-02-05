7428dbcc5cd5af7163b83f9e0f88e574723e6e3c
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-05 16:06:30 +0530

    add proper login fields

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
           <h3 style={{ margin: '15px' }}>
             Need an account? <Link to="/register">Create an account</Link>
           </h3>
-          <form className="mainform">
+          {/* <form className="mainform">
             <div className="form-group">
               <label htmlFor="exampleInputEmail1">Email address</label>
               <input
@@ -45,6 +45,44 @@ const Login = () => {
               className="btn btn-primary btn-lg"
             >
               Login
+            </button>
+          </form> */}
+
+          <form className="w-[100%] flex flex-col gap-2">
+            <input
+              type="text"
+              placeholder="First Name*"
+              minLength="3"
+              maxLength="32"
+              required
+            />
+            <input
+              type="text"
+              placeholder="Last Name*"
+              minLength="3"
+              maxLength="32"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              placeholder="Email*"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password*"
+            />
+            <input type="text" name="ssc" id="ssc" placeholder="SSC Marks" />
+            <input type="text" name="hsc" id="hsc" placeholder="HSC Marks" />
+            <button class="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-purple-50 text-purple-600 inline-block">
+              <span class="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-purple-600 group-hover:h-full opacity-90"></span>
+              <span class="relative group-hover:text-white text-2xl">
+                Login
+              </span>
             </button>
           </form>
         </div>
